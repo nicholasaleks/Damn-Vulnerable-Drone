f5d2ea076a5f3b2bbf5b7af5839e7b419fb4b46c
nicholasaleks <nicholas.aleks@gmail.com>
2024-01-18 22:57:28 -0500

    Completed connectivity between simulator, flight controller and companion computer

--- flight-controller/init.sh
+++ flight-controller/init.sh
@@ -3,12 +3,10 @@
 sudo rm -rf /sockets/socket.sock
 sudo socat pty,link=/dev/ttyACM0,raw,echo=0 unix-listen:/sockets/socket.sock,reuseaddr,fork &
 
-sleep 5
+sleep 15
 
 sudo chmod a+rw /dev/ttyACM0
 sudo chmod a+rw /sockets/socket.sock
 echo 'Initializing Flight Controller...'
 
-# Tools/autotest/sim_vehicle.py -v ArduCopter -f gazebo-iris --no-mavproxy --sim-address=10.13.0.5 -A '--uartA=uart:/dev/ttyACM0:57600'
-
-tail -f /dev/null
+Tools/autotest/sim_vehicle.py -v ArduCopter -f gazebo-iris --no-mavproxy --sim-address=10.13.0.5 -A '--uartA=uart:/dev/ttyACM0:57600'
