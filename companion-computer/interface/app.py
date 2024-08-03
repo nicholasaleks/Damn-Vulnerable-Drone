fd1b761b5e952b957a56a6c8e01f5100b6cc115b
nicholasaleks <nicholas.aleks@gmail.com>
2024-08-02 21:39:40 -0400

    Added all tamper attacks

--- companion-computer/interface/app.py
+++ companion-computer/interface/app.py
@@ -163,6 +163,11 @@ def initialize_udp_destinations():
             gcs_destination = UdpDestination(ip='10.13.0.4', port=14550)
 
         db.session.add(gcs_destination)
+
+        # Add default connection to QGC
+        qgc_destination = UdpDestination(ip='10.13.0.6', port=14550)
+        db.session.add(qgc_destination)
+
         db.session.commit()
 
 
